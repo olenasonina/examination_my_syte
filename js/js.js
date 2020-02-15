7e3cd0d1e413927bf88a90ae9b386d00b195d8c8
SoninVI <v.sonin1986@gmail.com>
2020-02-15 17:27:16 +0200

    ready 90%

--- js/js.js
+++ js/js.js
@@ -4,5 +4,25 @@ $('.our__work-slider').slick({
     speed: 300,
     slidesToShow: 1,
     centerMode: true,
-    variableWidth: true
+    variableWidth: true,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: false
+          // centerMode: true,
+          // centerPadding: '40px',
+          // slidesToShow: 3
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: false
+        //   centerMode: true,
+        //   centerPadding: '40px',
+        //   slidesToShow: 1
+        }
+      }
+    ]
   });
